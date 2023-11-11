ed2ba7e1e514612ac2cc9f472c0ed478b3562b3c
GabrielAraujo989 <ga.padua92@gmail.com>
2023-11-11 11:21:17 -0300

    (Alterações no Header transformando em um botão de navegação e no documento README)

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PI-PocketPlus
 Repositório destinado ao projeto do PI do 3° semestre DSM. 
+### Integrantes:
+* Bruno Vinícius de Laia Silva 
+* Felipe Carvalho Tardivo
+* Gabriel Araujo de Padua
+* Marlon Vinicius de Souza
+
+## Front-End
+O front-end é responsável por criar a interface gráfica e interativa da aplicação, onde o usuário pode realizar
+
+## Back-End
+## Documentação
+
+
--- front-end/pocket-plus/src/components/Header.jsx
+++ front-end/pocket-plus/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "../styles/header.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-function Header() {
+function Header({...props}) {
+  const nav = useNavigate();
+
   return (
     <div>
       <div className="header">
-        <img src="/logo.png" alt="logo" />
+      <Link to="/Dashboard">
+        <img src="/logo.png" alt="logo" /></Link>
 
       </div>
     </div>
